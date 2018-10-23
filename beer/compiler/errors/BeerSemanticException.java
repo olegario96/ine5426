8a507643d60ba77a58054673797afaf2bb846a40
Olegario96 <gustavo-olegario@hotmail.com>
2018-10-22 23:32:49 -0200

    Removing warning from ID

--- beer/compiler/errors/BeerSemanticException.java
+++ beer/compiler/errors/BeerSemanticException.java
@@ -1,6 +1,7 @@
 package beer.compiler.errors;
 
 public class BeerSemanticException extends Exception {
+    private static final long serialVersionUID = 12345678L;
     public BeerSemanticException(String message) {
         super(message);
     }
